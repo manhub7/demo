f5e60208c29f138717cc92b3ff0d9db230785ded
Maryam <shopmarij2@gmail.com>
2026-07-28 13:46:20 -0700

    Update app message

--- test/app.test.ts
+++ test/app.test.ts
@@ -5,7 +5,7 @@ app.use(express.json());
 
 // Health check route
 app.get('/health', (_req: Request, res: Response) => {
-  res.status(200).json({ status: 'ok' });
+  res.status(400).json({ status: 'ok' });
 });
 
 // Greeting route
